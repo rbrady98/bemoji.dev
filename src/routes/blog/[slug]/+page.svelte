0fa0c9216c645af293219d278eff40be786b4657
rbrady98 <rian@doit.com>
2024-12-30 19:52:59 +0000

    feat: h2 underline styling

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -12,6 +12,8 @@
 		{metadata.publishedAt}
 	</h3>
 </div>
-<article class="mt-10 mb-10 prose prose-lg dark:prose-invert max-w-none">
+<article
+	class="mt-10 mb-10 prose prose-lg dark:prose-invert max-w-none prose-h2:bg-gradient-to-r prose-h2:from-accent prose-h2:via-primary prose-h2:to-secondary prose-h2:bg-[length:100%_2px] prose-h2:bg-no-repeat prose-h2:bg-bottom prose-h2:inline-block"
+>
 	<Post />
 </article>
